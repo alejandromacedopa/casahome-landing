274c906c2d6bfbadaccce940e260b574448ae56f
alejandromacedop <macedoalejandro12@gmail.com>
2025-10-15 15:58:24 -0500

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-
+import vercel from '@astrojs/vercel/static'
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: true })],
+  output: 'static',
+  integrations: [],
+  adapter: vercel(),
 })
