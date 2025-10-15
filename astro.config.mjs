ee29dcd807ae54da9282fcf941046298406c2dc5
alejandromacedop <macedoalejandro12@gmail.com>
2025-10-15 16:18:19 -0500

    fix: vercel analytics manual script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/static'
-import vercelAnalytics from '@vercel/analytics/astro'
 
 export default defineConfig({
   output: 'static',
   integrations: [
     tailwind({ applyBaseStyles: true }),
-    vercelAnalytics(), 
   ],
   adapter: vercel(),
 })
